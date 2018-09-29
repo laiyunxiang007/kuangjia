58a542e379bf36f051c72753b6b0dc5d1629c85f
laiyunxiangwork <294805447@qq.com>
2018-09-29 18:03:12 +0800

    //第二版

--- src/main/java/com/example/kuangjia/test.java
+++ src/main/java/com/example/kuangjia/test.java
@@ -3,6 +3,7 @@ package com.example.kuangjia;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -12,9 +13,15 @@ public class test {
 
     @RequestMapping("/a")
     @ResponseBody
-    public String a(){
-        log.error("你好");
-        return "nihao";
+    public String add(){
+        try {
+            log.error("你好");
+            long i=1/0;
+            return "nihao";
+        }catch (Exception e){
+            return "error";
+        }
+
     }
 }
 
